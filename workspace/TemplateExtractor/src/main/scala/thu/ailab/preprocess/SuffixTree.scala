d2d06e86274fa86654a480bf4bfeade40860ef7e
qjp <qjp-ch-mail@163.com>
2013-05-07 17:34:48 +0800

    Finish the first step of implementation of Suffix Tree and begin to work on the HTML version.

--- workspace/TemplateExtractor/src/main/scala/thu/ailab/preprocess/SuffixTree.scala
+++ workspace/TemplateExtractor/src/main/scala/thu/ailab/preprocess/SuffixTree.scala
@@ -12,12 +12,14 @@ object RenderChars {
   val CornerRight    = "└";
 }
 
-class SuffixTree(rawInputStr: String, val canonicalChar: String = "$") {
-  val inputStr = 
-    if (rawInputStr.indexOf(
-        rawInputStr(rawInputStr.length - 1)) < rawInputStr.length - 1)
-      rawInputStr + canonicalChar
-    else rawInputStr      
+class SuffixTree[T : Ordering](rawInputSeq: IndexedSeq[T], 
+    val nullEdge: T, 
+    val canonicalChar: T) {
+  val inputSeq =
+    if (rawInputSeq.indexOf(
+        rawInputSeq(rawInputSeq.length - 1)) < rawInputSeq.length - 1)
+      rawInputSeq :+ canonicalChar
+    else rawInputSeq
   abstract class Show {
     def accept(visitor: SuffixTreeVisitor)
   }
@@ -31,27 +33,29 @@ class SuffixTree(rawInputStr: String, val canonicalChar: String = "$") {
     def getEndIndex() = if (endNode.isLeaf) endPtr + 1 else fixEndIndex
     private var fixEndIndex: Int = 0
     var endNode: BaseNode = Leaf()
-    def getCharAt(idx: Int) = inputStr(beginIndex + idx)
+    def getCharAt(idx: Int) = inputSeq(beginIndex + idx)
     def splitEdge(edgeLen: Int, _beginIndex: Int) = {
       val iNode = InternalNode()
       //before any modification
       val save = sameExceptStart(iNode, beginIndex + edgeLen)
       fixEndIndex = beginIndex + edgeLen
       iNode.addEdge(_beginIndex)
-      iNode.addEdge(inputStr(fixEndIndex), save)
+      iNode.addEdge(inputSeq(fixEndIndex), save)
       endNode = iNode
     }
     def sameExceptStart(_parentNode: InternalNode, _beginIndex: Int) = {
       new Edge(_parentNode, _beginIndex, this.endNode, this.fixEndIndex)
     }
     def length = getEndIndex - beginIndex
-    def getEdgeString = inputStr.substring(beginIndex, getEndIndex)
+    def getEdgeSeq = inputSeq.slice(beginIndex, getEndIndex)
+    def getEdgeString = inputSeq.slice(beginIndex, getEndIndex).mkString(" ")
+    def getEdgeLength = getEdgeString.length
     def isClosed = endNode.isInstanceOf[InternalNode]
     
     def toAscii(prefix: String, maxEdgeLength: Int) = {
       getEdgeString + (endNode match {
         case node: InternalNode =>
-          RenderChars.HorizontalLine * (maxEdgeLength - length + 1) + 
+          RenderChars.HorizontalLine * (maxEdgeLength - getEdgeLength + 1) + 
           node.toAscii(prefix + " " * (maxEdgeLength + 1))
         case _ => ""
       })
@@ -92,16 +96,16 @@ class SuffixTree(rawInputStr: String, val canonicalChar: String = "$") {
     def getTotalLeafCount = getNumber() + 1
   }
   class InternalNode private(number: Int) extends BaseNode("node", number) {
-    val edges = new MHashMap[Char, Edge]
+    val edges = new MHashMap[T, Edge]
     var suffixLink: Option[InternalNode] = None 
-    def hasEdge(c: Char) = edges.contains(c)
+    def hasEdge(c: T) = edges.contains(c)
     def addEdge(idx: Int) = {
-      edges(inputStr(idx)) = new Edge(InternalNode.this, idx)
+      edges(inputSeq(idx)) = new Edge(InternalNode.this, idx)
     }
-    def addEdge(c: Char, edge: Edge) = {
+    def addEdge(c: T, edge: Edge) = {
       edges(c) = edge
     }
-    def getEdge(c: Char) = edges.get(c)
+    def getEdge(c: T) = edges.get(c)
     def getNodeLabel = "(" + name.filter(_.isDigit) + ")"
     
     val isLeaf = false
@@ -109,7 +113,7 @@ class SuffixTree(rawInputStr: String, val canonicalChar: String = "$") {
       val nodeLabel = getNodeLabel + (
           if (suffixLink.isDefined) "->" + suffixLink.get.getNodeLabel 
           else "")
-      val maxEdgeLength = edges.values.maxBy(_.length).length
+      val maxEdgeLength = edges.values.maxBy(_.getEdgeLength).getEdgeLength
       val prefixPadding = prefix + " " * nodeLabel.length
       nodeLabel + (if (edges.size == 1) {
         RenderChars.HorizontalLine * 2 +
@@ -117,7 +121,7 @@ class SuffixTree(rawInputStr: String, val canonicalChar: String = "$") {
       }
       else {
         (for ((edge, idx) <- edges.values.toList.sortBy(
-            edge => inputStr(edge.beginIndex)).zipWithIndex) yield {
+            edge => inputSeq(edge.beginIndex)).zipWithIndex) yield {
           if (idx == 0) {
             RenderChars.TJunctionDown + RenderChars.HorizontalLine + 
             edge.toAscii(prefixPadding + RenderChars.VerticalLine + " ", maxEdgeLength)
@@ -141,13 +145,14 @@ class SuffixTree(rawInputStr: String, val canonicalChar: String = "$") {
    */
   var endPtr = 0
   val root = InternalNode()
-  val activePoint = new {
-    var activeNode: InternalNode = root
-    var activeEdge: Char = '\0'
-    var activeLength: Int = 0
-    def tryInsert(c: Char) = {
+  val activePoint = new ActivePoint
+  class ActivePoint {
+    var activeNode = root
+    var activeEdge = nullEdge
+    var activeLength = 0
+    def tryInsert(c: T) = {
       var ret = false
-      if (activeEdge == '\0' && activeNode.hasEdge(c)) {
+      if (activeEdge == nullEdge && activeNode.hasEdge(c)) {
         activeEdge = c
         activeLength += 1
       } else {
@@ -162,9 +167,9 @@ class SuffixTree(rawInputStr: String, val canonicalChar: String = "$") {
       ret
     }
     def insertEdge(curEndIndex: Int, preInsertNode: Option[InternalNode]) = {
-      if (activeEdge == '\0' || activeLength == 0) {
+      if (activeEdge == nullEdge || activeLength == 0) {
         activeNode.addEdge(curEndIndex)
-        activeNode.getEdge(inputStr(curEndIndex)).get.endNode
+        activeNode.getEdge(inputSeq(curEndIndex)).get.endNode
       } else {
         val edge = activeNode.getEdge(activeEdge).get
         edge.splitEdge(activeLength, curEndIndex)
@@ -178,8 +183,8 @@ class SuffixTree(rawInputStr: String, val canonicalChar: String = "$") {
       if (activeNode == root) {
         if (activeLength > 0) {
           activeLength -= 1
-          if (activeLength == 0) activeEdge = '\0'
-          else activeEdge = inputStr(endPtr - remainder + 1)
+          if (activeLength == 0) activeEdge = nullEdge
+          else activeEdge = inputSeq(endPtr - remainder + 1)
           if (oldEdgeOption.isDefined)
             normalizeAfterSuffix(oldEdgeOption.get, 1)
         }
@@ -195,7 +200,7 @@ class SuffixTree(rawInputStr: String, val canonicalChar: String = "$") {
         val edge = activeNode.getEdge(activeEdge).get
         if (activeLength == edge.length && edge.isClosed) {
           activeNode = edge.endNode.asInstanceOf[InternalNode]
-          activeEdge = '\0'
+          activeEdge = nullEdge
           activeLength = 0
         }
       }
@@ -209,7 +214,7 @@ class SuffixTree(rawInputStr: String, val canonicalChar: String = "$") {
             assert(!newEdge.endNode.isLeaf)
             activeNode = newEdge.endNode.asInstanceOf[InternalNode]
             activeLength -= newEdge.length
-            if (activeLength == 0) activeEdge = '\0'
+            if (activeLength == 0) activeEdge = nullEdge
             else activeEdge = oldEdge.getCharAt(beginIndex + newEdge.length)
             doNormalize(beginIndex + newEdge.length)
           }
@@ -229,7 +234,7 @@ class SuffixTree(rawInputStr: String, val canonicalChar: String = "$") {
   private def insertSuffix(times: Int, 
       preInsertNode: Option[InternalNode]): Unit = {
     def doInsert = {
-      val c = inputStr(endPtr)
+      val c = inputSeq(endPtr)
       if (!activePoint.tryInsert(c)) {
         remainder += 1
         None
@@ -241,23 +246,20 @@ class SuffixTree(rawInputStr: String, val canonicalChar: String = "$") {
       }
     }
     if (times > 0) {
-      println(activePoint)
-      println("# remainder: " + remainder)
-      println("insert \'" + inputStr(endPtr) + "\' at " + endPtr)
-      val node = doInsert
-      translateToAscii
-      println
-      if (node.isDefined) {
-        node.get match {
-          case internalNode: InternalNode => insertSuffix(times - 1, Some(internalNode))
-          case leaf: Leaf => insertSuffix(times - 1, preInsertNode)
-        }
+      doInsert match {
+        case Some(internalNode: InternalNode) => insertSuffix(times - 1, Some(internalNode))
+        case Some(leaf: Leaf) => insertSuffix(times - 1, preInsertNode)
+        case _ =>
       }
     }
   }
   def build {
-    if (endPtr < inputStr.length()) {
+    if (endPtr < inputSeq.length) {
+      println(activePoint)
+      println("# remainder: " + remainder)
       insertSuffix(remainder, None)
+      println("insert %s at %d".format(inputSeq(endPtr), endPtr))
+      translateToAscii
       endPtr += 1
       build
     } else {
@@ -265,6 +267,7 @@ class SuffixTree(rawInputStr: String, val canonicalChar: String = "$") {
     }
   }
   build
+  assert(inputSeq.length == getLeafCount(root))
   def getLeafCount(node: InternalNode): Int = {
     (for (edge <- node.edges.values) yield {
       if (edge.isClosed)
@@ -308,10 +311,27 @@ class SuffixTree(rawInputStr: String, val canonicalChar: String = "$") {
   }
 }
 
+class HTMLSuffixTree(html: Array[String]) extends SuffixTree[String](html, "\0", "$") {
+  def findAllRepetitions() = {
+    for (key <- root.edges.keys.toList.sortBy(_.filter(_.isDigit))) {
+      val minDepth = key.filter(_.isDigit).toInt
+      val edge = root.getEdge(key).get
+      val edgeSeq = edge.getEdgeSeq
+      var rep = edgeSeq.takeWhile(_.filter(_.isDigit).toInt >= minDepth)
+      if (rep.length < edgeSeq.length) rep
+    } 
+  }
+}
 
 object TestSuffixTree extends App {
   val s = "dedododeeodo"
-  val tree = new SuffixTree(s)
-  println(tree.inputStr.length)
-  println(tree.getLeafCount(tree.root))
+  val t1 = new SuffixTree[Char](s, '\0', '$')
+  //val html = Array("html1", "head2", "meta3", "meta3", 
+  //    "body2", "div3", "a4", "div4", "img5", "div3", "a4", "div4", "img5", "div3")
+  val html = Array("body2", "div3", "a4", "div4", "img5", "div3", "a4", "div4", "img5", "div3")
+  val head = html(0)
+  val son = html(1)
+  //val html = Array("body1", "head2", "div3", "a4", "p4", "a4", "p4", "div3", "ul4", "li5", "li5", "li5")
+  val t2 = new SuffixTree[String](html, "\0", "$")
+  t2.translateToAscii
 }
